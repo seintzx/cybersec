f3007b40d6c855028eff27a0c67f8f0d740ef49c
seintzx <seintzx@gmail.com>
2019-09-13 11:33:31 +0200

    update work script, move python foldere

--- script/README.md
+++ script/README.md
@@ -1,3 +1,15 @@
 # scripts
 
-some usefull script, will explain their purpose when i'll have finished them
+Script I have created for their purposes.
+
+## apply-patch
+
+It's a script to apply vmware patch to VMware Tools after kernel update.
+
+## reverse-word
+
+Reverse the word order of a phrase.
+
+## update
+
+Script for Ubuntu's Update.
--- script/reverse-word.py
+++ script/reverse-word.py
@@ -1,7 +1,13 @@
-s = "Keeper,GladiatorTrainer,TortureChamber,Gate,Keeper,GladiatorTrainer,TortureChamber,Gate,Keeper,GladiatorTrainer,TortureChamber,Gate,Keeper,GladiatorTrainer,TortureChamber,Gate,Keeper,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,SoulExtractor,HeartOfDarkness,SoulExtractor,HeartOfDarkness,SoulExtractor,HeartOfDarkness,SoulExtractor,HeartOfDarkness,Keeper,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,Keeper,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,Keeper,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,Keeper,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,Keeper,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,TimeMachine,TimeMachine,TimeMachine,TimeMachine,TimeMachine,Keeper,Keeper,Keeper,Keeper,Keeper,GoldPit,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,GoldPit,GoldPit,GoldPit,GoldPit,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,GladiatorTrainer,GladiatorTrainer,GladiatorTrainer,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,TortureChamber,TortureChamber,Gate,SoulExtractor,HeartOfDarkness,GoblinPit,Gate,SoulExtractor,HeartOfDarkness"
-words = s.split(',') 
-string =[] 
-for word in words: 
-    string.insert(0, word) 
-  
-print(",".join(string)) 
+#!/usr/bin/env python3
+
+
+def rev_word(s):
+    s = "Your list of words to reverse here"
+    words = s.split(',')
+    string = []
+    for word in words:
+        string.insert(0, word)
+    print(",".join(string))
+
+if __name__ == "__main__":
+    rev_word("test it")
